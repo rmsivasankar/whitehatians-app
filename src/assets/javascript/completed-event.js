86d6c58f4937d65a0323a1d4182185f876ae2517
R M Sivasankar <143864327+rmsivasankar@users.noreply.github.com>
2024-08-10 15:50:53 +0530

    Update completed-event.js

--- src/assets/javascript/completed-event.js
+++ src/assets/javascript/completed-event.js
@@ -1,5 +1,11 @@
 import SEA from '../../assets/events/SEA.svg'
 const Comp_event = [
+  {
+        event_title:"MATRIXZ",
+        event_image:SEA,
+        event_description:"Capture the Flag (CTF) competitions are exercises in which participants, either individually or as part of a team, are challenged to find and exploit vulnerabilities in a system to capture a flag or piece of information.",
+        reg_link:"/matrixz-ctf"
+    },
 ]
 
 export default Comp_event;
