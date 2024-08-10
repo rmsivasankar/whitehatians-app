c39c3e4b08380ae71f97a659e588b20604a5680b
R M Sivasankar <143864327+rmsivasankar@users.noreply.github.com>
2024-08-10 15:49:31 +0530

    Update events-list.js

--- src/assets/javascript/events-list.js
+++ src/assets/javascript/events-list.js
@@ -1,11 +1,5 @@
 import SEA from '../../assets/events/SEA.svg'
 const Event_list = [
-    {
-        event_title:"MATRIXZ",
-        event_image:SEA,
-        event_description:"Capture the Flag (CTF) competitions are exercises in which participants, either individually or as part of a team, are challenged to find and exploit vulnerabilities in a system to capture a flag or piece of information.",
-        reg_link:"/matrixz-ctf"
-    },
 ]
 
 export default Event_list;
