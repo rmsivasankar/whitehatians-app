849d61d83a133bb0ee427bed37401b72a8ac1da6
R M Sivasankar <143864327+rmsivasankar@users.noreply.github.com>
2024-08-10 16:05:24 +0530

    Update events-list.js

--- src/assets/javascript/events-list.js
+++ src/assets/javascript/events-list.js
@@ -1,5 +1,11 @@
-import SEA from '../../assets/events/SEA.svg'
+import SEA1 from '../../assets/events/SEA1.png'
 const Event_list = [
+  {
+        event_title:"ZYVERSE",
+        event_image:SEA1,
+        event_description:"A symposium in a college is an organized event that typically includes a series of technical and non-technical activities aimed at fostering learning, creativity, and collaboration among students.",
+        reg_link:"/Nil"
+    },
 ]
 
 export default Event_list;
