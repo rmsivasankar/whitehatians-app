556984fb4aa31e2d7fe4e92c7202c2100d88a639
R M Sivasankar <143864327+rmsivasankar@users.noreply.github.com>
2024-08-13 08:51:40 +0530

    Update events-list.js

--- src/assets/javascript/events-list.js
+++ src/assets/javascript/events-list.js
@@ -4,7 +4,7 @@ const Event_list = [
         event_title:"ZYVERSE",
         event_image:SEA1,
         event_description:"A symposium in a college is an organized event that typically includes a series of technical and non-technical activities aimed at fostering learning, creativity, and collaboration among students.",
-        reg_link:"zyverse.whitehatians.in"
+        reg_link:"https://zyverse.whitehatians.in/"
     },
 ]
 
