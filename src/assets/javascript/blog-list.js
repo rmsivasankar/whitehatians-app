8d9ad791085b0b4010d9c061d5171fa6ba5059aa
R M Sivasankar <143864327+rmsivasankar@users.noreply.github.com>
2024-08-17 21:42:02 +0530

    Update blog-list.js

--- src/assets/javascript/blog-list.js
+++ src/assets/javascript/blog-list.js
@@ -3,11 +3,19 @@ import wh from '../../assets/Logo/Logo.png'
 const Blog_list = [
     {
         image:wh,
+        tag:"Event",
+        desc:"Department of Cybersecurity Whitehatians club has organized a 3rd state level CTF event named Matrixz a Jeopardy Style CTF.",
+        lin_link:"https://www.linkedin.com/posts/whitehatians_srmvecctf-matrixzctf-ctfchallenge-activity-7230154539213930496-gdrH?utm_source=share&utm_medium=member_desktop",
+        icon:wh,
+        date:"August 9 2024"
+    },
+    {
+        image:wh,
         tag:"Workshop",
         desc:"Department of Cybersecurity Whitehatians club has organized a workshop on network threats and attack to the students by Dhivakar Security Engineer at Hebesec Technologies.",
-        lin_link:"link",
+        lin_link:"https://www.linkedin.com/posts/whitehatians_srm-valliammai-engineering-college-the-activity-7188178732518445056-Ab1d?utm_source=share&utm_medium=member_desktop",
         icon:wh,
-        date:"August 5 2024"
+        date:"April 22 2024"
     },
 ]
 
