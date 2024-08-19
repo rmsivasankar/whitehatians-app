5a73c0e05f813ec6602465613558e1395d672309
R M Sivasankar <143864327+rmsivasankar@users.noreply.github.com>
2024-08-19 13:37:40 +0530

    Update Matrixz.jsx

--- src/Components/pages/Events/Ctf/Matrixz.jsx
+++ src/Components/pages/Events/Ctf/Matrixz.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Matrixz.css'
-import banner from '../../../../assets/events/1.jpg'
 import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
 import RuleIcon from '@mui/icons-material/Rule';
 import { Helmet } from 'react-helmet';
@@ -13,68 +12,58 @@ const Matrixz = () => {
         <div>
             <Helmet>
                 <meta charSet="utf-8" />
-                <title>Matrixz CTF</title>
+                <title>CODEWARS 2.0</title>
                 <link rel="canonical" href="https://www.whitehatians.in/matrixz-ctf" />
             </Helmet>
         </div>
             <div className='ctf'>
-                <h2>MATRIXZ CTF</h2>
+                <h2>CODEWARS 2.0</h2>
                 <div className='ctf-section'>
                     <div className='ctf-event'>
                         <div className='ctf-about'>
-                            <h3>ABOUT THE EVENT</h3>
-                            <h4>JEOPARDY CTF</h4>
+                            <h3>STYLING + ANIMATION</h3>
+                            <h4>Evaluvation Criteria</h4>
                             <ul>
-                                <li>WEB EXPLOITATION</li>
-                                <li>OSINT</li>
-                                <li>CRYPTOGRAPHY</li>
-                                <li>REVERSE ENGINEERING</li>
-                                <li>FORENSICS</li>
-                                <li>BINARY EXPLOITATION</li>
+                                <li>CODE QUALITY</li>
+                                <li>DESIGN AND AESTHETICS</li>
+                                <li>FUNCTIONALITY</li>
+                                <li>PERFORMANCE</li>
+                                <li>ACCESSIBILITY</li>
                             </ul>
                         </div>
                     </div>
                     <div className='ctf-duration'>
                         <div className='prize'>
-                            <h3>PRIZE POOL <span style={{color:"red"}}>₹4,500</span></h3>
+                            <h3>PRIZE POOL <span style={{color:"red"}}>₹2,000</span></h3>
                             <ul style={{color: "yellowgreen"}}>
                                 <li>CASH PRIZE</li>
-                                <li>REFRESHMENTS WILL BE PROVIDED TO ALL PARTICIPANTS</li>
-                            </ul>
-                        </div>
-                        <div className='prize'>
-                            <h3>ENTRY FEE <ConfirmationNumberIcon style={{color: "skyblue"}} /> </h3>
-                            <ul style={{color: "yellowgreen"}}>
-                                <li>Per Team : <span style={{color: "red"}}>₹150</span></li>
+                                <li>FOOD WILL BE PROVIDED TO ALL PARTICIPANTS</li>
                             </ul>
                         </div>
                         <div className='time'>
                             <h3>EVENT DURATION</h3>
-                            <p>6 Hours CTF</p>
+                            <p>2 Hours</p>
                             <ul>
-                                <li>9th August 2024</li>
-                                <li>Starts at 9 AM</li>
-                                <li>Ends at 3 PM</li>
-                                <li>OLD SEMINAR HALL 2ND FLOOR</li>
+                                <li>24th August 2024</li>
+                                <li>Starts at 10:30 AM</li>
+                                <li>Ends at 12:00 PM</li>
+                                <li>LAB 7 3rd FLOOR</li>
                             </ul>
                         </div>
                     </div>
                     <div className='ctf-banner'>
-                        <div className='banner'>
-                            <img src={banner} />
-                        </div>
                         <div className='prize'>
                             <h3>ENTRY RULES <RuleIcon style={{color: "skyblue"}} /></h3>
                             <ul style={{color: "yellowgreen"}}>
-                                <li>Do not attempt to exploit systems outside of the competition environment.</li>
-                                <li>Work with your team without sharing solutions externally.</li>
-                                <li>Avoid using excessive automated tools that might disrupt the challenge.</li>
-                                <li>Do not target other participants or interfere with their activities.</li>
-                                <li>Disclose any discovered vulnerabilities only through official channels.</li>
+                                <li>Eligibility: Open to all students and web enthusiasts.</li>
+                                <li>HTML Markup: Modify only the provided HTML; no removal of elements.</li>
+                                <li>Tools: Use any CSS frameworks or JS libraries for enhancements.</li>
+                                <li>Design: Ensure responsiveness across devices and creative animations.</li>
+                                <li>Submission: Submit all files (HTML, CSS, JS) by the deadline.</li>
                             </ul>
                         </div>
                         <div className='register-btn'>
-                            <Link target='blank_' to="https://forms.gle/p96GZ42XLqWKYmFh9">Register Now</Link>
+                            <Link target='blank_'>Download Files</Link>
                         </div>
                     </div>
                 </div>
@@ -83,18 +72,20 @@ const Matrixz = () => {
                         <h3>FOR QUEREIES CONTACT:</h3>
                         <ul>
                             <li>+91 9042568655</li>
-                            <li>+91 7904747081</li>
-                            <li>+91 6381098798</li>
                         </ul>
                     </div>
-                    <div className='coordinator-details'>
-                        {Coord_list.map((cord_list,index) => {
-                            return <div className='c-details'>
+                    {
+                        /*
+                            <div className='coordinator-details'>
+                                {Coord_list.map((cord_list,index) => {
+                                return <div className='c-details'>
                                     <img key={index} src={cord_list.avatar} />
                                     <p key={index}>{cord_list.name}</p>
-                        </div>
-                        })}
-                    </div>
+                                    </div>
+                                })}
+                            </div>
+                         */
+                    }
                 </div>
             </div>
         </>
