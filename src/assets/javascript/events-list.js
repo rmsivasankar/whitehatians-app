dbf1bd6c14761310a9a48cb4df7b5829e3343a0d
R M Sivasankar <143864327+rmsivasankar@users.noreply.github.com>
2024-08-21 12:37:31 +0530

    Update events-list.js

--- src/assets/javascript/events-list.js
+++ src/assets/javascript/events-list.js
@@ -6,6 +6,12 @@ const Event_list = [
         event_description:"A symposium in a college is an organized event that typically includes a series of technical and non-technical activities aimed at fostering learning, creativity, and collaboration among students.",
         reg_link:"https://zyverse.whitehatians.in/"
     },
+  {
+        event_title:"HACKOPS",
+        event_image:SEA1,
+        event_description:"A Capture the Flag (CTF) competition is a type of cybersecurity exercise where participants, typically teams or individuals, attempt to find and extract “flags” – secret text strings – hidden in purposefully vulnerable programs, websites, or networks.",
+        reg_link:"/hackops"
+    },
 ]
 
 export default Event_list;
