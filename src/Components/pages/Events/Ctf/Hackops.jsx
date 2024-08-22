e680d1eb301fc4767e2d0a33db0b8776fcd2564f
R M Sivasankar <143864327+rmsivasankar@users.noreply.github.com>
2024-08-22 15:37:39 +0530

    Update Hackops.jsx

--- src/Components/pages/Events/Ctf/Hackops.jsx
+++ src/Components/pages/Events/Ctf/Hackops.jsx
@@ -86,7 +86,7 @@ const Matrixz = () => {
                             <li>Aravind: +91 7904747081</li>
                         </ul>
                     </div>
-                    {*
+                    { /*
                         <div className='coordinator-details'>
                                 {Coord_list.map((cord_list,index) => {
                                 return <div className='c-details'>
@@ -95,7 +95,7 @@ const Matrixz = () => {
                                     </div>
                                 })}
                             </div>
-                    *}
+                    */ }
                 </div>
             </div>
         </>
