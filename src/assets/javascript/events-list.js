06f5591f7ff885a92fa3d94448ecc3a43a18390f
R M Sivasankar <143864327+rmsivasankar@users.noreply.github.com>
2024-08-27 11:31:43 +0530

    Update events-list.js

--- src/assets/javascript/events-list.js
+++ src/assets/javascript/events-list.js
@@ -1,12 +1,5 @@
-import SEA1 from '../../assets/events/SEA1.png'
 import HACKOPS from '../../assets/events/HACKOPS.png'
 const Event_list = [
-  {
-        event_title:"ZYVERSE",
-        event_image:SEA1,
-        event_description:"A symposium in a college is an organized event that typically includes a series of technical and non-technical activities aimed at fostering learning, creativity, and collaboration among students.",
-        reg_link:"https://zyverse.whitehatians.in/"
-    },
   {
         event_title:"HACKOPS",
         event_image:HACKOPS,
