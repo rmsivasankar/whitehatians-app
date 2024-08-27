67edbeda0d6c6ea601b2f4786b93022d5f3ac315
R M Sivasankar <143864327+rmsivasankar@users.noreply.github.com>
2024-08-27 11:31:12 +0530

    Update completed-event.js

--- src/assets/javascript/completed-event.js
+++ src/assets/javascript/completed-event.js
@@ -1,5 +1,12 @@
 import SEA from '../../assets/events/SEA.svg'
+import SEA1 from '../../assets/events/SEA1.png'
 const Comp_event = [
+  {
+        event_title:"ZYVERSE",
+        event_image:SEA1,
+        event_description:"A symposium in a college is an organized event that typically includes a series of technical and non-technical activities aimed at fostering learning, creativity, and collaboration among students.",
+        reg_link:"https://zyverse.whitehatians.in/"
+    },
   {
         event_title:"MATRIXZ",
         event_image:SEA,
