41cc1f8a1235a58a4e3f6cf9f96975b85bae960f
R M Sivasankar <143864327+rmsivasankar@users.noreply.github.com>
2024-09-16 12:52:37 +0530

    Update events-list.js

--- src/assets/javascript/events-list.js
+++ src/assets/javascript/events-list.js
@@ -1,11 +1,4 @@
-import HACKOPS from '../../assets/events/HACKOPS.png'
 const Event_list = [
-  {
-        event_title:"HACKOPS",
-        event_image:HACKOPS,
-        event_description:"A Capture the Flag (CTF) competition is a type of cybersecurity exercise where participants, typically teams or individuals, attempt to find and extract “flags” – secret text strings – hidden in purposefully vulnerable programs, websites, or networks.",
-        reg_link:"/hackops"
-    },
 ]
 
 export default Event_list;
