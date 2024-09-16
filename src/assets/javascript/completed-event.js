82b7e31c13ce752cc27b2fedd2b766af52db0b89
R M Sivasankar <143864327+rmsivasankar@users.noreply.github.com>
2024-09-16 12:52:49 +0530

    Update completed-event.js

--- src/assets/javascript/completed-event.js
+++ src/assets/javascript/completed-event.js
@@ -1,5 +1,6 @@
 import SEA from '../../assets/events/SEA.svg'
 import SEA1 from '../../assets/events/SEA1.png'
+import HACKOPS from '../../assets/events/HACKOPS.png'
 const Comp_event = [
   {
         event_title:"ZYVERSE",
@@ -12,6 +13,12 @@ const Comp_event = [
         event_image:SEA,
         event_description:"Capture the Flag (CTF) competitions are exercises in which participants, either individually or as part of a team, are challenged to find and exploit vulnerabilities in a system to capture a flag or piece of information.",
         reg_link:"/matrixz-ctf"
+    },
+  {
+        event_title:"HACKOPS",
+        event_image:HACKOPS,
+        event_description:"A Capture the Flag (CTF) competition is a type of cybersecurity exercise where participants, typically teams or individuals, attempt to find and extract “flags” – secret text strings – hidden in purposefully vulnerable programs, websites, or networks.",
+        reg_link:"/hackops"
     },
 ]
 
