2c65616085fb99889cac2e21247c4e9a3687475b
R M Sivasankar <143864327+rmsivasankar@users.noreply.github.com>
2024-09-30 09:04:00 +0530

    Update blog-list.js

--- src/assets/javascript/blog-list.js
+++ src/assets/javascript/blog-list.js
@@ -3,11 +3,51 @@ import wh from '../../assets/Logo/Logo.png'
 const Blog_list = [
     {
         image:wh,
-        tag:"Workshop",
-        desc:"Department of Cybersecurity Whitehatians club has organized a workshop on network threats and attack to the students by Dhivakar Security Engineer at Hebesec Technologies.",
+        tag:"Technical Event",
+        desc:"Department of Cybersecurity Whitehatians club has organized a HACKOPS, a 8-hour Capture the Flag (CTF) competition.Participants demonstrated exceptional skills and determination as they navigated through intricate challenges, solving puzzles, and capturing flags in an intense battle of wits and strategy.",
         lin_link:"link",
         icon:wh,
-        date:"August 5 2024"
+        date:"Septmeber 9 2024"
+    },
+     {
+        image:wh,
+        tag:"Technical Event",
+        desc:"Department of Cybersecurity Whitehatians club has organized a MATRIXZ, a 6-hour Capture the Flag (CTF) competition.Participants demonstrated exceptional skills and determination as they navigated through intricate challenges, solving puzzles, and capturing flags in an intense battle of wits and strategy.",
+        lin_link:"https://www.linkedin.com/feed/update/urn:li:share:7230154537968316416/?actorCompanyId=96657438",
+        icon:wh,
+        date:"August 9 2024"
+    },
+    {
+        image:wh,
+        tag:"Workshop",
+        desc:"Department of Cybersecurity Whitehatians club has organized a workshop on network threats and attack to the students by Dhivakar Security Engineer at Hebesec Technologies.",
+        lin_link:"https://www.linkedin.com/feed/update/urn:li:ugcPost:7188178731209822209/?actorCompanyId=96657438",
+        icon:wh,
+        date:"April 22 2024"
+    },
+    {
+        image:wh,
+        tag:"Technical Event",
+        desc:" The Department of Cyber Security, SRM Valliammai Engineering College conducted a technical event named CODEWARS. Over 50 students from the various departments have participated and showcased their talents in programming, particularly in problem-solving and algorithm challenges. Participants were required to solve problems within a designated time frame. ",
+        lin_link:"https://www.linkedin.com/feed/update/urn:li:ugcPost:7171005868581355520/?actorCompanyId=96657438",
+        icon:wh,
+        date:"February 28 2024"
+    },
+    {
+        image:wh,
+        tag:"Awareness Program",
+        desc:" The Department Cyber Security of SRM VALLIAMMAI ENGINEERING COLLEGE has conducted a Cyber security hands on awareness program for students of jrk global senior secondary school, Maraimalai nagar.",
+        lin_link:"https://www.linkedin.com/feed/update/urn:li:ugcPost:7165822742989139968/?actorCompanyId=96657438",
+        icon:wh,
+        date:"February 2 2024"
+    },
+    {
+        image:wh,
+        tag:"Seminar",
+        desc:"The Department of Cyber Security from SRM Valliammai Engineering College, in collaboration with the Institute of Engineers - India, hosted a significant one-day seminar on Cyber Crime Awareness (in Tamil).",
+        lin_link:"https://www.linkedin.com/feed/update/urn:li:ugcPost:7165822742989139968/?actorCompanyId=96657438",
+        icon:wh,
+        date:"October 18 2023"
     },
 ]
 
